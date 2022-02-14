4a28f79970bb625eb7622710cf72d9109ad53072
Julian Lazaras <lazaras_cat@cat.pdx.edu>
2022-02-14 10:36:42 -0800

    so I don't forget to submit, still have to do 4

--- hw3/05-background.js
+++ hw3/05-background.js
@@ -1,3 +1,25 @@
-// Enter your code here
-
-console.log('Enter your code here');
+const params = new Proxy(new URLSearchParams(window.location.search), {
+  get: (searchParams, prop) => searchParams.get(prop),
+});
+var run = (params.Stop == null) ? true : false;
+//var interval = (params.interval == null) ? 5000 : params.interval;
+var intervalid = null;
+function stop() {
+    run = !run;
+    if(run) {
+        document.getElementById("interval").disabled = true;
+        let interval = document.getElementById("interval").value;
+        start_changer(interval);
+    }else{
+        document.getElementById("interval").disabled = false;
+        clearInterval(intervalid);
+    }
+    document.getElementById("stopbtn").value = (run == true) ? "Stop" : "Restart";
+}
+function start_changer(interval) {
+    intervalid = setInterval(function() { 
+       let color = "rgb(" + Math.floor(Math.random() * 255).toString() + ", " + Math.floor(Math.random() * 255).toString() + ", " + Math.floor(Math.random() * 255).toString() + ")";
+    console.log("color: " + color);
+       document.getElementById("background").style.backgroundColor = color; 
+    }, interval*1000); 
+}
