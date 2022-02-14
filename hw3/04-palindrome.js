01801d0c43101e7b8895d7e6b400d029c921dd37
Julian Lazaras <lazaras_cat@cat.pdx.edu>
2022-02-14 13:29:03 -0800

    commiting hw3

--- hw3/04-palindrome.js
+++ hw3/04-palindrome.js
@@ -1,3 +1,26 @@
-// Enter your code here
+function is_palindrome(text) {
+  let count_in = (text.length % 2 == 0) ? text.length / 2 : (text.length - 1) / 2;
+  let idx = 0;
+  console.log("count_in: " + count_in);
+  while(idx < count_in) {
+    console.log("index: " + idx + " from left: " + text[idx] + " from right: " + text[text.length - idx -1]);
+    if(text[idx] != text[text.length - idx -1]){ return false; }
+    idx++;
+  }
+  return true;
+}
 
-console.log('Enter your code here');
+function pal_check() {
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+       get: (searchParams, prop) => searchParams.get(prop),
+    });
+    let val = params.number;
+    if(val != null) {
+    document.getElementById("number").value = val;
+    let res = is_palindrome(val);
+    console.log("result: " + res); 
+    if(res) {
+        document.getElementById("result").innerText = "Yes";
+    }
+    }
+}
